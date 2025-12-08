d055075f4432071a9f6ba953bb2782b7248967b1
Snehal <snehal.webroottech@gmail.com>
2025-12-08 23:20:23 +0530

    old cokkie removed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -296,25 +296,25 @@
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDXbrQhbrrdjSTVaPzJeILiZP_asMgUIrw&callback=initMap"></script> --}}
 
     <script>
-        window.addEventListener("load", function() {
-            window.cookieconsent.initialise({
-                palette: {
-                    popup: {
-                        background: "#000"
-                    },
-                    button: {
-                        background: "#f1d600"
-                    }
-                },
-                theme: "classic",
-                content: {
-                    message: "This website uses cookies to ensure you get the best experience.",
-                    dismiss: "Got it!",
-                    link: "Learn more",
-                    href: "/privacy-policy"
-                }
-            })
-        });
+        // window.addEventListener("load", function() {
+        //     window.cookieconsent.initialise({
+        //         palette: {
+        //             popup: {
+        //                 background: "#000"
+        //             },
+        //             button: {
+        //                 background: "#f1d600"
+        //             }
+        //         },
+        //         theme: "classic",
+        //         content: {
+        //             message: "This website uses cookies to ensure you get the best experience.",
+        //             dismiss: "Got it!",
+        //             link: "Learn more",
+        //             href: "/privacy-policy"
+        //         }
+        //     })
+        // });
     </script>
 
     <script>
